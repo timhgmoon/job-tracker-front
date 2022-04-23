aa26bdaabd2d3c606b2ec4be1b34d9ed9104a0e3
Timothy Moon <timhgmoon@gmail.com>
2022-04-22 21:58:55 -0700

    basic login format

--- job-tracker/src/Login.js
+++ job-tracker/src/Login.js
@@ -1,19 +1,44 @@
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import FormControl from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import KeyIcon from '@mui/icons-material/Key';
+import TextField from '@mui/material/TextField';
+
 const axios = require('axios');
-axios.post('https://personal-job-tracker-api.herokuapp.com/sign-in/', {
-  "user": {
-  "email": "tim@gmail.com",
-  "password": "Tim1507025"
-  }
-})
-.then(function(res) {
-  console.log(res);
-})
-.catch(function (error) {
-  console.log(error);
-});
+// axios.post('https://personal-job-tracker-api.herokuapp.com/sign-in/', {
+//   "user": {
+//   "email": "tim@gmail.com",
+//   "password": "Tim1507025"
+//   }
+// })
+// .then(function(res) {
+//   console.log(res);
+// })
+// .catch(function (error) {
+//   console.log(error);
+// });
 const Login = () => {
   return (
-     <h1>Login</h1>
+     <Container maxWidth>
+      <Box>
+        <FormControl variant="standard">
+        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+          <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+          <TextField id="input-with-sx" label="Username" variant="standard" />
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+          <KeyIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+          <TextField id="input-with-sx" label="Password" variant="standard" />
+        </Box>
+        <Button sx={{mt: 2}}variant="outlined">Log In</Button>
+        </FormControl>
+      </Box>
+     </Container>
   );
 }
 
